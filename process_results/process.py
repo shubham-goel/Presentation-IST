1435f21fd18ea28cc110f79b18ad1d024192ecb2
Shubham Goel <sgoel160@gmail.com>
2016-06-29 08:33:12 +0200

    Beginning to process

---
+++ process_results/process.py
@@ -0,0 +1,28 @@
+import math
+from scipy.special import comb
+from decimal import *
+
+import time
+import pickle
+import itertools
+import subprocess
+import numpy as np
+
+def load_from_file(filename):
+	file = open(filename, 'r')
+	return  pickle.load(file)
+
+for n in range(4,8):
+	m=n
+	e=int(2.5*n)
+	t=n
+	l=n
+
+	filename = 'dat_files/n{}-m{}-e{}-t{}-l{}.dat'.format(n,m,e,t,l)
+	(params,rt_dat,prob) = load_from_file(filename)
+	
+	assert params['n'] == n
+	assert params['e'] == e
+	assert params['m'] == m
+	assert params['t'] == t
+	assert params['l'] == l
