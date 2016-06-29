7a5dc35b46484bfd887a52e676501cd5d139ded9
Shubham Goel <sgoel160@gmail.com>
2016-06-29 09:30:14 +0200

    Added scaling, runtime results

--- process_results/process.py
+++ process_results/process.py
@@ -18,11 +18,34 @@ for n in range(4,8):
 	t=n
 	l=n
 
-	filename = 'dat_files/n{}-m{}-e{}-t{}-l{}.dat'.format(n,m,e,t,l)
+	filename = 'MONTE_CARLO_dat_files/n{}-m{}-e{}-t{}-l{}.dat'.format(n,m,e,t,l)
 	(params,rt_dat,prob) = load_from_file(filename)
-	
+
 	assert params['n'] == n
 	assert params['e'] == e
 	assert params['m'] == m
 	assert params['t'] == t
 	assert params['l'] == l
+
+	print n,rt_dat['2m'],rt_dat['2mt']
+print ''
+print ''
+print ''
+print ''
+print ''
+for n in range(11,41):
+	m=n
+	e=int(2.5*n)
+	t=n
+	l=n
+
+	filename = 'MONTE_CARLO_dat_files/n{}-m{}-e{}-t{}-l{}.dat'.format(n,m,e,t,l)
+	(params,rt_dat,prob) = load_from_file(filename)
+
+	assert params['n'] == n
+	assert params['e'] == e
+	assert params['m'] == m
+	assert params['t'] == t
+	assert params['l'] == l
+
+	print n,rt_dat['2m'],rt_dat['2mt']
